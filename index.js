a564497af96da4433ce207ae5804a0abfdb13b41
nathanbegbie <nathanbegbie@gmail.com>
2020-02-21 14:56:02 +0000

    fix string issue

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ async function run() {
     const respondableId = core.getInput("respondableId", { required: true });
     // const response = core.getInput("response", { required: true });
 
-    const response = "![welcome to our community](https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2F4.bp.blogspot.com%2F-llh6c4dYACc%2FT7qnnu8xI7I%2FAAAAAAAACHA%2Foi4dzQ-TInI%2Fs1600%2Fyay.png&f=1&nofb=1 "Welcome to our community")"
+    const response = 'We want to say ![welcome to our community](https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2F4.bp.blogspot.com%2F-llh6c4dYACc%2FT7qnnu8xI7I%2FAAAAAAAACHA%2Foi4dzQ-TInI%2Fs1600%2Fyay.png&f=1&nofb=1 "Welcome to our community")'
 
     const author = core.getInput("author", { required: true });
     const token = process.env.GITHUB_TOKEN;
