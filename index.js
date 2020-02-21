f9deae85325806be4bd623e6ce651a391d32bdb9
nathanbegbie <nathanbegbie@gmail.com>
2020-02-21 14:54:08 +0000

    add image markdown to the message

--- index.js
+++ index.js
@@ -12,7 +12,10 @@ function parseAuthors(input) {
 async function run() {
   try {
     const respondableId = core.getInput("respondableId", { required: true });
-    const response = core.getInput("response", { required: true });
+    // const response = core.getInput("response", { required: true });
+
+    const response = "![welcome to our community](https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2F4.bp.blogspot.com%2F-llh6c4dYACc%2FT7qnnu8xI7I%2FAAAAAAAACHA%2Foi4dzQ-TInI%2Fs1600%2Fyay.png&f=1&nofb=1 "Welcome to our community")"
+
     const author = core.getInput("author", { required: true });
     const token = process.env.GITHUB_TOKEN;
     const exemptedAuthors = parseAuthors(core.getInput("exemptedAuthors"));
